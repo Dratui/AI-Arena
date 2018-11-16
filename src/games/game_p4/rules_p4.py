855ccc809470b30d4718199cfae1e9ccb041235a
2018louysa <arnaud.louys@student-cs.fr>
2018-11-16 15:36:21 +0100

    convert grid to board object for p4

--- src/games/game_p4/rules_p4.py
+++ src/games/game_p4/rules_p4.py
@@ -1,27 +1,26 @@
-def make_a_move(grid, player, move):
+def make_a_move(board, player, move):
     """update the grid to acknowledge the last move"""
     i= 0
-    while grid[i][move] == " ":
+    while board.read_tile(i,move) == " ":
         if i < len(grid)-1:
             i+=1
         else:
             i+=1
             break
-    grid[i-1][move] = player
-    return grid
+    board.change_tile(i-1,move, player)
 
 def is_over(grid, move, player):
     """check if the last move played makes the player who played it win,
     and return in the second argument the number of the winning player"""
     i= 0
-    while grid[i][move] == " ":
+    while board.read_tile(i,move) == " ":
         i+=1
     list_near_points = [] #list of the number of the player payns in each direction starting from the last one beginning with up then going clockwise
     directions = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
     for dir in directions:
         k=0
         while i+dir[0]*k >= 0 and i+dir[0]*k < len(grid) and move+k*dir[1] >= 0 and move+k*dir[1] < len(grid[0]):
-            if grid[i+dir[0]*k][move+k*dir[1]] == player:
+            if board.read_tile(i+dir[0]*k,move+k*dir[1]) == player:
                 k+=1
             else:
                 break
