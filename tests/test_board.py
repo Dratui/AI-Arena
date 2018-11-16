ab2d0fcb13c5f4781d6e720bc298bd8175b69390
Marco Boucas <marco.boucas@student-cs.fr>
2018-11-16 15:06:30 +0100

    Added some tests in the test for the board, now available to test transposes functions

--- tests/test_board.py
+++ tests/test_board.py
@@ -4,5 +4,24 @@ import pytest
 def test_change_tile():
 	A=Board(4,4)
 	A.change_tile(1,1,"Salut")
-	assert A.grid==[[' ',' ',' '],[' ','Salut',' '],[' ',' ',' ']]
+	assert A.grid==[[' ',' ',' ',' '],[' ','Salut',' ',' '],[' ',' ',' ',' '],[' ',' ',' ',' ']]
 
+
+def test_read_tile():
+	A=Board(4,4)
+	A.change_tile(1,1,"Salut")
+	assert A.read_tile(1,1)=="Salut"
+
+def test_transpose_grid_clockwise():
+	A=Board(2,2)
+	A.change_tile(1,1,"Bonjour")
+	B=A.transpose_grid_clockwise()
+	C=[[" "," "],["Bonjour"," "]]
+	assert B==C
+
+def test_transpose_grid_anticlockwise():
+	A=Board(2,2)
+	A.change_tile(1,1,"Bonjour")
+	B=A.transpose_grid_anticlockwise()
+	C=[[" ","Bonjour"],[" "," "]]
+	assert B==C
