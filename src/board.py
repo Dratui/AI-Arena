44317fb3fa83f510194341ad8eb4de587005f1d9
Grégoire Sanson <gregoire.sanson@student-cs.fr>
2018-11-16 11:32:21 +0100

    Added display

--- src/board.py
+++ src/board.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 class Board:
     def __init__(self, height, width):
         self.height = height
         self.width = width
         self.grid = [[' ' for i in range(self.width)] for j in range(self.height)]
-
-        
+    
+    
     def change_tile(self, row, col,value):
         """
         CHANGE ONE VALUE OF THE GRID
@@ -16,6 +18,7 @@ class Board:
         """
         self.grid[row][col]=value
         
+        
     def read_tile(self, row, col):
         """
         READ ONE VALUE OF THE GRID
@@ -26,13 +29,22 @@ class Board:
         """
         return self.grid[row][col]
 
+
     def transpose_grid_clockwise(self):
+        """
+        TRANSPOSE THE BOARD CLOCKWISE
+        """
         return [list(elem) for elem in zip(*self.grid[::-1])]
         
+        
     def transpose_grid_anticlockwise(self):
+        """
+        TRANSPOSE THE BOARD ANTICLOCKWISE (CHIBRALLY)
+        """
         self.transpose_grid_clockwise()
         self.transpose_grid_clockwise()
         return self.transpose_grid_clockwise()
+        
         
     def read_row(self, x):
         """
@@ -43,6 +55,7 @@ class Board:
         """
         return self.grid[row]
         
+        
     def read_column(self, col):
         """
         READ ONE COLUMN OF THE GRID
@@ -51,7 +64,43 @@ class Board:
         Returns the column as a list
         """
         return [self.grid[i][col] for i in range(self.height)]
-
-
     
+    
+    def get_all_tiles(self):
+        """
+        RETURNS A LIST CONTAINING EVERY TILE OF THE GRID
+        """
+        list_tiles = []
+        for i,j in range(self.height):
+            for j in range(self.width):
+                list_tiles.append(self.read_tile(i,j))
+        return list_tiles
+    
+        
+    def grid_to_string_with_size(self, maxsize):
+        """
+        RETURNS A STRING MODELLING THE GRID, TAKING ACCOUNT OF THE BIGGEST VALUE TO AVOID SHIFT
+        maxsize : size of the biggest number/value displayed 
+        """
+        width = self.width
+        height = self.height
+        list_tiles = self.get_all_tiles
+        grid_str = """"""
+        sticks = """ ==="""*width + """ \n"""
+        string_grid = sticks
+        lines_str = []
+        for i in range(height) :
+            line = """|"""
+            numbers = [str(list_tiles[width*i+j]).ljust(n) for j in range(width)]
+            line = line + "|".join(numbers)
+            line = line + """|\n"""
+            lines_str.append(line)
+        grid_str = sticks.join(lines_str)
+        grid_str = sticks + grid_str + """ ==="""*width + """ """
+        return grid_str
+        
+    
+    
+    
+
     
