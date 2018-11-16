c95586ac2030179820e25f17583812762d6c1090
Quentin Mondon-Cancel <M.Cancel.Quentin@gmail.com>
2018-11-16 10:56:17 +0000

    Added a function to calculate the rank of every player given their score

--- src/tournament.py
+++ src/tournament.py
@@ -1,15 +1,17 @@
-
+import operator
 
 class Tournament:
     """This is the class that is going to manage tournaments, it includes all necessary information"""
     number_players = 0
     game = "" #Holds the name of the game
     list_players_who_is=[] #This list gives the list of players (with the number of each player being the position in the list) and states the corresponding AI or human
-    leaderboard=[]
+    score=[] #score[i] holds the number of matches that player i has won
+    leaderboard=[] #Leaderboard[i] hold the rank of player i.
     #The following three hold the modules that correspond to the game being played
     grid = None
     player_interaction = None
     rules = None
+    matches = [[]] #Is a nested list where matches[x][y] is an integer where -1 means the game between players x and y has not been played yet and otherwise this integer being the winner of the match
     
     def tournament_init(self):
         """Starts a new tournament, retrieving all relevant information on the game and the players"""
@@ -22,10 +24,14 @@ class Tournament:
             import_2048(self)
         if self.game == "p4" : #Same
             import_p4(self)
-        self.leaderboard = [(0,i) for i in range(self.number_players)]
-        
+        self.score = [0] * self.number_players
+        matches = []
+        for i in range(self.number_players) :
+            tempRow=[-1]*self.number_players
+            matches.append(tempRow)
         
     def import_2048(self):
+        """Imports the game 2048 into the attributes of self"""
         import src.games.game_2048.grid_2048
         self.grid = src.games.game_2048.grid_2048
         import src.games.game_2048.player_interaction_2048
@@ -34,6 +40,7 @@ class Tournament:
         self.rules = src.games.game_2048.rules_2048
 
     def import_p4(self):
+        """Imports the game p4 into the attributes of self"""
         import src.games.game_p4.grid_p4
         self.grid = src.games.game_p4.grid_p4
         import src.games.game_p4.player_interaction_p4
@@ -43,6 +50,7 @@ class Tournament:
         
         
 def select_player(number_player):
+    """Asks a human who every player is, whether a human of the name of an AI"""
     who_is_player = []
     number_human_players = 1
     for i in range(1,number_player+1):
@@ -54,6 +62,21 @@ def select_player(number_player):
         who_is_player.append(temp_player)
     return who_is_player
     
-
-
-
+def calculate_leaderboard(score):
+    """Recieves the list of the score of every player and returns the rank of each player"""
+    size = len(score)
+    copy_score=score
+    leaderboard = [0]*size
+    index_of_max, current_max = max(enumerate(copy_score), key=operator.itemgetter(1)) #The index of the max value and said max value
+    current_rank = 1
+    current_rank_without_equal = 1 #This variable will always be incremented as it will give the value current_rank must take after managing two players with the same rank (E.G. to have 1,1,3 as ranks and not 1,1,2)
+    while 0 in leaderboard :
+        leaderboard[index_of_max] = current_rank
+        copy_score[index_of_max] = -1
+        index_of_max, current_max_temp = max(enumerate(copy_score), key=operator.itemgetter(1))
+        current_rank_without_equal += 1
+        if current_max_temp != current_max :
+            current_max = current_max_temp
+            current_rank = current_rank_without_equal
+    return leaderboard
+        
