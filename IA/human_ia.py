fed7299bd7221a2085e35e8d1344d09c269130ef
maxbe <max.beckermann@student-cs.fr>
2018-11-19 11:58:45 +0100

    rework of the human_ia

--- IA/human_ia.py
+++ IA/human_ia.py
@@ -2,7 +2,7 @@ from src.games.games import Game
 
 def ia_output(board,game): #the ia's output corresponds to the human input
     output=input(game.move_description)
-    while output not in [str(game.map_move_to_input[i]) for i in range(len(game.move_available))]: #intput verification
+    while output not in [str(game.map_move_to_input[i]) for i in game.get_move_effective()]: #intput verification
         output=input(game.move_description)
     return(game.map_input_to_move[output])
 
