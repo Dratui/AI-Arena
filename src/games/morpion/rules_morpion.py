59b22c9eae0940532ee3b52c728cefa8ca63247b
2018louysa <arnaud.louys@student-cs.fr>
2018-11-19 11:59:10 +0100

    created morpion game

---
+++ src/games/morpion/rules_morpion.py
@@ -0,0 +1,30 @@
+def make_a_move(board,x,y,player):
+    if board.read_tile(x,y) == " ":
+        board.change_tile(x,y,player)
+
+def move_effective(board):
+    list = []
+    for i in range(3):
+        for j in range(3):
+            if board.read_tile(i,j) == " ":
+                list.append((i,j))
+    return list
+
+def is_over(board, move, player):
+    i= 0
+    while board.read_tile(i,move) == " ":
+        i+=1
+    list_near_points = [] #list of the number of the player payns in each direction starting from the last one beginning with up then going clockwise
+    directions = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+    for dir in directions:
+        k=0
+        while i+dir[0]*k >= 0 and i+dir[0]*k < board.width and move+k*dir[1] >= 0 and move+k*dir[1] <board.width:
+            if board.read_tile(i+dir[0]*k,move+k*dir[1]) == player:
+                k+=1
+            else:
+                break
+        list_near_points.append(k-1)
+    for k in range(4):
+        if list_near_points[k]+list_near_points[k+4] >1 :
+            return True
+    return False
