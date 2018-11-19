8a05ce73328b359516d0461c7607e23e686ca142
2018louysa <arnaud.louys@student-cs.fr>
2018-11-19 10:03:38 +0100

    added choice of board for display_board

--- src/games/games.py
+++ src/games/games.py
@@ -1,9 +1,11 @@
 import src.games.game_2048.board_2048 as board_2048
 import src.games.game_2048.player_interaction_2048 as player_interaction_2048
 import src.games.game_2048.rules_2048 as rules_2048
+
 import src.games.game_p4.grid_p4 as grid_p4
 import src.games.game_p4.player_interaction_p4 as player_interaction_p4
 import src.games.game_p4.rules_p4 as rules_p4
+
 from src.board import Board
 
 
@@ -18,24 +20,27 @@ class Game:
         - next_turn : modifies the board according to what is supposed to happen when a player finishes his turn but don't return it (i.e. 2k48 --> spawn a new tile)
         - display_board : returns the string used to display the board
     """
-    def __init__(self):
-        self.name = "" #name of the game
-        self.list_board = [] #list of the boards (one for each players)
-        self.score = []
-        self.list_player = [] #list of the id of players (0,1,2,...)
-        self.move_available = [] #list of the inputs available
-        self.board_size = (0,0)
-        self.is_over_function = lambda x: None
-        self.make_a_move_function = lambda x: None
-        self.move_description = "" #what would be displayed to help the player uderstand what input corresponds to which move
-        self.map_input_to_move = {} #convert input to move number i.e. {'h' : 0,'d' : 1,'b' : 2,'g' : 3}
-        self.map_move_to_input = {} #convert move number to input i.e. {0 : 'h',1 : 'd',2 : 'b',3 : 'g'}
-        self.is_board_equal = False #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
-        self.theme_number = 0 #theme_number if needed
-        self.player_playing = 0 #id of the player which is currently playing
-        self.next_turn_function = lambda x: None
-        self.calc_score_function = lambda x: None
-        self.max_char_size = 2
+    def __init__(self, name = "", number_player = 2, move_available = [], board_size = (4,4), make_a_move_function = lambda x: None, is_over_function = lambda x: None,  next_turn_function = lambda x: None,
+                calc_score_function = lambda x: None, move_description = "", map_input_to_move = {}, map_move_to_input = {}, is_board_equal = False , theme_number = 0,
+                player_playing = 0, max_char_size = 2):
+
+        self.name = name#name of the game
+        self.list_board = [Board(board_size[0], board_size[1]) for i in range(number_player)] #list of the boards (one for each players)
+        self.score = [0 for i in range(number_player)]
+        self.list_player = [i for i in range(number_player)] #list of the id of players (0,1,2,...)
+        self.move_available = move_available #list of the inputs available
+        self.board_size = board_size
+        self.is_over_function = is_over_function
+        self.make_a_move_function = make_a_move_function
+        self.move_description = move_description #what would be displayed to help the player uderstand what input corresponds to which move
+        self.map_input_to_move = map_input_to_move #convert input to move number i.e. {'h' : 0,'d' : 1,'b' : 2,'g' : 3}
+        self.map_move_to_input = map_move_to_input #convert move number to input i.e. {0 : 'h',1 : 'd',2 : 'b',3 : 'g'}
+        self.is_board_equal = is_board_equal #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
+        self.theme_number = theme_number #theme_number if needed
+        self.player_playing = player_playing #id of the player which is currently playing
+        self.next_turn_function = next_turn_function
+        self.calc_score_function = calc_score_function
+        self.max_char_size = max_char_size
 
     def get_board(self, player = 0):
         return self.list_board[player]
@@ -50,8 +55,8 @@ class Game:
     def is_over(self, *args):
         return (self.is_over_function(self.list_board[self.player_playing],*args), self.player_playing)
 
-    def display_board(self, *args):
-        return self.list_board[0].grid_to_string_with_size(self.max_char_size)
+    def display_board(self, board_number = 0, *args):
+        return self.list_board[i].grid_to_string_with_size(self.max_char_size)
 
     def next_turn(self, *args):
         self.next_turn_function(self.list_board[self.player_playing])
