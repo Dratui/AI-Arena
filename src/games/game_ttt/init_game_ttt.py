a8aa02ab3b8f502fb6efd517032d8e69a1617baf
Desdra <45032247+Desdra@users.noreply.github.com>
2018-11-19 16:53:30 +0100

    Update init_game_ttt.py

--- src/games/game_ttt/init_game_ttt.py
+++ src/games/game_ttt/init_game_ttt.py
@@ -14,19 +14,11 @@ def create_ttt(new_game):
             for j in range(3):
                 move_available.append((i,j))
         new_game.move_available = move_available#list of the input available
-<<<<<<< HEAD:src/games/game_ttt/init_game_ttt.py
-        new_game.is_over_function = rules_ttt.is_over
-        new_game.make_a_move_function = rules_ttt.make_a_move
-        new_game.move_description = player_interaction_ttt.move_description # ex :"d : droite, g : gauche, h : haut, b : bas" what would be displayed to help the player uderstand what input corresponds to which move
-        new_game.map_input_to_move = {}
-        new_game.map_move_to_input = {}
-=======
         new_game.is_over_function = rules_morpion.is_over
         new_game.make_a_move_function = rules_morpion.make_a_move
         new_game.move_description = player_interaction_morpion.move_description # ex :"d : droite, g : gauche, h : haut, b : bas" what would be displayed to help the player uderstand what input corresponds to which move
         new_game.map_input_to_move = {"1,1" : 0,'1,2' : 1,'1,3' : 2,'2,1' : 3,'2,2' : 4,'2,3' : 5,'3,1' : 1,'3,2' : 2,'3,3' : 8}
         new_game.map_move_to_input = {0 : "1,1",1 : '1,2',2 : '1,3',3 : '2,1',4 : '2,2',5 : '2,3',6 : '3,1',7 : '3,2',8 : '3,3'}
->>>>>>> 264fd5f4c6f456854c828e4045c7fd412656a59c:src/games/game_morpion/init_game_morpion.py
         new_game.is_board_equal = True #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
         new_game.next_turn_function = lambda x: x
         new_game.move_effective_function = rules_ttt.move_effective
