827a981c844b215a53b02ac09f00e4e10d0880ed
maxbe <max.beckermann@student-cs.fr>
2018-11-19 10:02:35 +0100

    bug fix in the human ia

--- IA/random_ia.py
+++ IA/random_ia.py
@@ -2,7 +2,7 @@ from src.games.games import Game
 from random import randint
 
 def ia_output(board,game): # this ia randomize's its output
-    return (game.map_move_to_input[randint(0,len(game.move_available))])
+    return (game.map_intput_to_move[randint(0,len(game.move_available))])
 
 
 
