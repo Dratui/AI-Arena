13c41b41ecbe30d0a51cc7c1156d5cc2a958b7dc
2018louysa <arnaud.louys@student-cs.fr>
2018-11-19 12:01:36 +0100

    example for morpion started

--- example_init_game.py
+++ example_init_game.py
@@ -9,6 +9,7 @@ import src.games.game_2048.rules_2048 as rules_2048
 
 g2k_game = init_game("2048")
 gp4_game = init_game("p4")
+morpion_game = init_game("morpion")
 
 game = "p4"
 
@@ -37,6 +38,18 @@ def launch():
                 gp4_game.change_player()
             else:
                 print("Le joueur ",  gp4_game.player_playing, "gagne")
+                break
+    if game == "morpion":
+        while 1:
+            print("joueur", morpion_game.player_playing)
+            print(morpion_game.list_board[0].grid_to_string_with_size(3))
+            move = player_interaction_p4.get_player_move(morpion_game.list_board[0])
+            morpion_game.make_a_move(move)
+            if not(morpion_game.is_over(move, morpion_game.player_playing)[0]):
+                morpion_game.next_turn()
+                morpion_game.change_player()
+            else:
+                print("Le joueur ",  morpion_game.player_playing, "gagne")
                 break
 
 if __name__ == "__main__" :
