bcc41b9a3976ba8990fd03f5ed7003755bb5521f
2018louysa <arnaud.louys@student-cs.fr>
2018-11-19 10:52:28 +0100

    added init_game in game folders

---
+++ src/games/game_p4/init_game_p4.py
@@ -0,0 +1,22 @@
+import src.games.game_p4.grid_p4 as grid_p4
+import src.games.game_p4.player_interaction_p4 as player_interaction_p4
+import src.games.game_p4.rules_p4 as rules_p4
+from src.games.games import *
+from src.board import Board
+
+
+def create_p4():
+        new_game = Game()
+        new_game.name = "p4"
+        new_game.score = [0,0]
+        new_game.list_board = [Board(vertical_size, horizontal_size) for i in range(2)]
+        new_game.list_player = [0,1]
+        new_game.board_size = (vertical_size,horizontal_size)
+        new_game.move_available = [i for i in range(horizontal_size)] #list of the input available
+        new_game.is_over_function = rules_p4.is_over
+        new_game.make_a_move_function = rules_p4.make_a_move
+        new_game.move_description = player_interaction_p4.move_description # ex :"d : droite, g : gauche, h : haut, b : bas" what would be displayed to help the player uderstand what input corresponds to which move
+        new_game.map_input_to_move = {}
+        new_game.map_move_to_input = {}
+        new_game.is_board_equal = True #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
+        new_game.next_turn_function = lambda x: x
--- src/games/games.py
+++ src/games/games.py
@@ -1,10 +1,12 @@
 import src.games.game_2048.board_2048 as board_2048
 import src.games.game_2048.player_interaction_2048 as player_interaction_2048
 import src.games.game_2048.rules_2048 as rules_2048
+import src.games.game_2048.init_game_2048 as init_game_2048
 
 import src.games.game_p4.grid_p4 as grid_p4
 import src.games.game_p4.player_interaction_p4 as player_interaction_p4
 import src.games.game_p4.rules_p4 as rules_p4
+import src.games.game_p4.init_game_p4 as init_game_p4
 
 from src.board import Board
 
@@ -79,39 +81,13 @@ class Game:
 def init_game(name, vertical_size = 6, horizontal_size = 6, players_number = 2):
     """Create a new game with the name given in args"""
     if name == "2048":
-        new_game = Game()
-        new_game.name = "2048"
-        new_game.score = [0 for i in range(players_number)]
-        new_game.list_board = [board_2048.init_board(Board(4,4)) for i in range(players_number)]
-        new_game.list_player = [i for i in range(players_number)]
-        new_game.board_size = (4,4)
-        new_game.move_available = [0,1,2,3] #list of the input available
-        new_game.is_over_function = rules_2048.is_over
-        new_game.make_a_move_function = rules_2048.make_a_move
-        new_game.move_description = player_interaction_2048.move_description # ex :"d : droite, g : gauche, h : haut, b : bas" what would be displayed to help the player uderstand what input corresponds to which move
-        new_game.map_input_to_move = {'h' : 0,'d' : 1,'b' : 2,'g' : 3}
-        new_game.map_move_to_input = {0 : 'h',1 : 'd',2 : 'b',3 : 'g'}
-        new_game.is_board_equal = False #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
-        new_game.next_turn_function = rules_2048.create_new_tile
-        new_game.calc_score_function = rules_2048.calc_score
-        new_game.move_effective_function = rules_2048.move_effective
+        return init_game_2048.create_2048()
 
     elif name == "p4":
-        new_game = Game()
-        new_game.name = "p4"
-        new_game.score = [0,0]
-        new_game.list_board = [Board(vertical_size, horizontal_size) for i in range(2)]
-        new_game.list_player = [0,1]
-        new_game.board_size = (vertical_size,horizontal_size)
-        new_game.move_available = [i for i in range(horizontal_size)] #list of the input available
-        new_game.is_over_function = rules_p4.is_over
-        new_game.make_a_move_function = rules_p4.make_a_move
-        new_game.move_description = player_interaction_p4.move_description # ex :"d : droite, g : gauche, h : haut, b : bas" what would be displayed to help the player uderstand what input corresponds to which move
-        new_game.map_input_to_move = {}
-        new_game.map_move_to_input = {}
-        new_game.is_board_equal = True #set it to true if the board of both player must be the same (i.e. in the Puissance 4 game)
-        new_game.next_turn_function = lambda x: x
+        return init_game_p4.create_p4()
     else:
         raise Exception("Jeu non reconnu")
 
-    return new_game
+
+def test():
+    return 1
