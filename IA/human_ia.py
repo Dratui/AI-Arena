0d47300616f9d653c9e36b85662a1605fc452f31
maxbe <max.beckermann@student-cs.fr>
2018-11-19 09:51:18 +0100

    bug fix in the human ia

--- IA/human_ia.py
+++ IA/human_ia.py
@@ -4,7 +4,7 @@ def ia_output(board,game): #the ia's output corresponds to the human input
     output=input(game.move_description)
     while output not in [str(game.map_move_to_input[i]) for i in range(len(game.move_available))]: #intput verification
         output=input(game.move_description)
-    return(output)
+    return(game.map_move_to_input(output))
 
 
 
