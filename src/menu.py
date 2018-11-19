7f31d3300e215a91c4cfe9185c1e47284f065501
Boucas Marco <marco.boucas@student-cs.fr>
2018-11-19 15:10:46 +0100

    Affichage du menu du jeu

--- src/menu.py
+++ src/menu.py
@@ -4,8 +4,9 @@ from tkinter import font
 TILES_FONT = ("Verdana", 40, "bold")
 
 fenetre = Tk()
-canvas = Canvas(fenetre, bg="#FFCC99", width=900, height=900,cursor="target")
-photo = PhotoImage(file="../menu.png")
+fenetre.title("AI Arena")
+canvas = Canvas(fenetre, width=900, height=700, cursor="target")
+photo = PhotoImage(file="../graphics/menu.PNG")
 
 
 
@@ -13,16 +14,6 @@ def afficher():
     canvas.create_image(0, 0, anchor=NW, image=photo)
     canvas.pack()
 
-    Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
-    Frame1.pack(side=LEFT, padx=30, pady=30)
-
-    # frame 2
-    Frame2 = Frame(fenetre, borderwidth=2, relief=GROOVE)
-    Frame2.pack(side=LEFT, padx=10, pady=10)
-
-    #Creation du frame qui sera a l'interieur
-    Button(Frame1, text ='Launch the Game',font=TILES_FONT,cursor="target")
-    Button(Frame2, text ='Leave',font=TILES_FONT,cursor="target")
 
 def clavier(event):
     touche = event.keysym
