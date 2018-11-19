2189dcd3129f85d51963651c38550cd133f8735a
Grégoire Sanson <gregoire.sanson@student-cs.fr>
2018-11-19 15:30:20 +0100

    Added p4 graphics

--- src/graphics_p4.py
+++ src/graphics_p4.py
@@ -2,7 +2,9 @@ from tkinter import *
 
 ##
 
-TILES_FG_COLOR = {' ':"#ffffff", 0:"#ff0000", 1:"#ff8000"}
+TILES_FG_COLOR = {' ':"#ffffff", 0:"#ff0000", 1:"#ffff00"}
+
+TILES_FONT = ("Verdana", 20, "bold")
 
 ##
 
@@ -10,6 +12,7 @@ grid_size = (10,10)
 
 game_grid = [[' ',1,0,1,0,1,' ',0,0,' '] for i in range(10)]
 list_canvas = [[' ' for j in range(10)] for i in range(10)]
+list_buttons = [' ' for j in range(grid_size[1])]
 
 fenetre = Tk()
 
@@ -21,9 +24,33 @@ def afficher():
             case = Canvas(fenetre, bg = "#ffffff", height = 50, width = 50)
             case.grid_propagate(0)
             case.grid(column = y, row = x)
-            text = Label(case, text = 'O', fg = TILES_FG_COLOR[game_grid[x][y]])
+            text = Label(case, text = 'O', fg = TILES_FG_COLOR[game_grid[x][y]], bg = "#ffffff", font = TILES_FONT)
             text.place(x=25, y=25, anchor="center")
+    for y in range(grid_size[1]) :
+        button = Button(fenetre, text = "Ici", height = 3, width = 6, command = lambda arg = y : clic(arg))
+        button.grid_propagate(0)
+        button.grid(column = y, row = 0)
+        list_buttons[y] = button
+        
+
+def clic(i):
+    
+    print("blabla")
+    global game_grid
+    
+    if i == 0 :
+        game_grid = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] for i in range(10)]
+    
+    elif i == 1 :
+        game_grid = [[0,0,0,0,0,0,0,0,0,0] for i in range(10)]
+        
+    elif i == 2 :    
+        game_grid = [[1,1,1,1,1,1,1,1,1,1] for i in range(10)]
+        
+    print(game_grid)
+    afficher()
+    fenetre.update()
 
 afficher()
-# fenetre.bind("<Key>", clavier_0)
+
 fenetre.mainloop()
