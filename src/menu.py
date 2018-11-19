13b7225b280857209ea3c7c22c7cd2cbe0e31dd9
Boucas Marco <marco.boucas@student-cs.fr>
2018-11-19 15:39:56 +0100

    Menu file, very beautiful

--- src/menu.py
+++ src/menu.py
@@ -8,23 +8,20 @@ fenetre.title("AI Arena")
 canvas = Canvas(fenetre, width=900, height=700, cursor="target")
 photo = PhotoImage(file="../graphics/menu.PNG")
 
-
-
-def afficher():
-    canvas.create_image(0, 0, anchor=NW, image=photo)
-    canvas.pack()
-
-
+canvas.create_image(0, 0, anchor=NW, image=photo)
+canvas.create_text(450,260,text="Play",font=TILES_FONT,fill="#de8421",activefill="#ff3300") 
+canvas.pack()
+def clic_play(events):
+    print("Play")
 def clavier(event):
     touche = event.keysym
     for i in range(4):
         for j in range(4):
             game_grid[i][j]="0"
-    afficher()
+    
 
     fenetre.update()
 
-afficher()
 canvas.focus_set()
 canvas.pack()
 
