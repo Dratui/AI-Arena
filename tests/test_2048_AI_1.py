1d35dbf72453e853aa003b337099233dbe6060ea
Quentin Mondon-Cancel <M.Cancel.Quentin@gmail.com>
2018-11-21 17:07:10 +0000

    Ajout de tests

--- tests/test_2048_AI_1.py
+++
@@ -1,5 +0,0 @@
-import AI.2048_AI_1 as AI
-from pytest import *
-
-def test_ai_output():
-    assert ai_output([[None, None, None, None], [None, None, None, None], [None, None, None, None], [2, None, None, 2]], "2048") in [0,1,2,3]
