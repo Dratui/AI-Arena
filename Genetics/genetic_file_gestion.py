80432a1c4a00d68b8044c8a40348f3aa664fc511
amalcor <amalio.coron@student-cs.Fr>
2018-11-21 11:12:30 +0100

    Genetics finalized

--- Genetics/genetic_file_gestion.py
+++ Genetics/genetic_file_gestion.py
@@ -6,36 +6,42 @@ FILE_NAME=['filename','file_name',"namefile","name_file",'file']
 SCORE_FUNCTION=["score_function",'scorefunction','score']
 
 def do_work_for_genetic(worktodo):
+    """
+    Execution to a text file
+    :param worktodo: a texte file with commands to execute
+    :return: 
+    """
     source_file = open(worktodo,'r')
-
+    lines_source_file=[]
     for line in source_file:
         if not(line in ['\n',' \n',' ','','\n ']):
-            parameters=line.replace('\n','').split(' ') #parameters must be : genetic_algorithm population_number moves_number generation_number score_function=
-            i=1
-            dict_parameters={}
-            while i<len(parameters):
-                #print(dict_parameters)
-                if(parameters[i] in  POP_NUMB):
-                    dict_parameters[POP_NUMB[0]]=parameters[i+1]
-                    i=i+1
-                elif(parameters[i] in MOVES_NUMB):
-                    dict_parameters[MOVES_NUMB[0]]=parameters[i+1]
-                    i=i+1
-                elif(parameters[i] in GENE_NUMB):
-                    dict_parameters[GENE_NUMB[0]]=parameters[i+1]
-                    i=i+1
-                elif(parameters[i] in FILE_NAME):
-                    dict_parameters[FILE_NAME[0]]=parameters[i+1]
-                    i=i+1
-                elif(parameters[i] in SCORE_FUNCTION):
-                    dict_parameters[SCORE_FUNCTION[0]]=parameters[i+1]
-                    i=i+1
-                i=i+1
-            print(dict_parameters)
-            print(dict_parameters[FILE_NAME[0]])
-            results_file = open(dict_parameters[FILE_NAME[0]]+'.txt' ,'a+')
-            import Genetics.score_functions
-            score_function=getattr(Genetics.score_functions,dict_parameters[SCORE_FUNCTION[0]])
-            results_file.write("Results of {} for {} = {} and {} = {} and {} = {} with {} = {} are {}. \n \n".format(parameters[0],MOVES_NUMB[0],dict_parameters[MOVES_NUMB[0]],POP_NUMB[0],dict_parameters[POP_NUMB[0]],GENE_NUMB[0],dict_parameters[GENE_NUMB[0]],SCORE_FUNCTION[0],dict_parameters[SCORE_FUNCTION[0]],genetic_algorithm(int(dict_parameters[MOVES_NUMB[0]]),int(dict_parameters[POP_NUMB[0]]),int(dict_parameters[GENE_NUMB[0]]),score_function)))
+            lines_source_file.append(line)
 
-do_work_for_genetic('to_do_commands.txt')
+    for line in source_file:
+        parameters=line.replace('\n','').split(' ') #parameters must be : genetic_algorithm population_number moves_number generation_number score_function=
+        i=1
+        dict_parameters={}
+        while i<len(parameters):
+            #print(dict_parameters)
+            if(parameters[i] in  POP_NUMB):
+                dict_parameters[POP_NUMB[0]]=parameters[i+1]
+                i=i+1
+            elif(parameters[i] in MOVES_NUMB):
+                dict_parameters[MOVES_NUMB[0]]=parameters[i+1]
+                i=i+1
+            elif(parameters[i] in GENE_NUMB):
+                dict_parameters[GENE_NUMB[0]]=parameters[i+1]
+                i=i+1
+            elif(parameters[i] in FILE_NAME):
+                dict_parameters[FILE_NAME[0]]=parameters[i+1]
+                i=i+1
+            elif(parameters[i] in SCORE_FUNCTION):
+                dict_parameters[SCORE_FUNCTION[0]]=parameters[i+1]
+                i=i+1
+            i=i+1
+        print(dict_parameters)
+        print(dict_parameters[FILE_NAME[0]])
+        results_file = open(dict_parameters[FILE_NAME[0]]+'.txt' ,'a+')
+        import Genetics.score_functions
+        score_function=getattr(Genetics.score_functions,dict_parameters[SCORE_FUNCTION[0]])
+        results_file.write("Results of {} for {} = {} and {} = {} and {} = {} with {} = {} are {}. \n \n".format(parameters[0],MOVES_NUMB[0],dict_parameters[MOVES_NUMB[0]],POP_NUMB[0],dict_parameters[POP_NUMB[0]],GENE_NUMB[0],dict_parameters[GENE_NUMB[0]],SCORE_FUNCTION[0],dict_parameters[SCORE_FUNCTION[0]],genetic_algorithm(int(dict_parameters[MOVES_NUMB[0]]),int(dict_parameters[POP_NUMB[0]]),int(dict_parameters[GENE_NUMB[0]]),score_function)))
--- Genetics/score_functions.py
+++ Genetics/score_functions.py
@@ -1,6 +1,13 @@
-def calc_score(list_board, current_player):
+def score_simple_sum(list_board, current_player):
     sum = 0
     for i in list_board[current_player].get_all_tiles():
-        if i!=' ':
+        if i!=None:
             sum += int(int(i))
+    return sum
+
+def score_eulerian(list_board, current_player):
+    sum = 0
+    for i in list_board[current_player].get_all_tiles():
+        if i!=None:
+            sum += int(int(i))**2
     return sum
