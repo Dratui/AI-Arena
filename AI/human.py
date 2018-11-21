d35ab55ef0627e68be20507deb0fd896c9469a25
maxbe <max.beckermann@student-cs.fr>
2018-11-21 11:51:23 +0100

    small change to human

--- AI/human.py
+++ AI/human.py
@@ -4,7 +4,8 @@ import time
 
 def ai_output(board,game):
     board.input=None
-    while board.input==None:
+    while board.input==None or game.map_input_to_move[board.input] not in game.get_move_effective():
         time.sleep(0.5)
     return(game.map_input_to_move[board.input])
+
 
