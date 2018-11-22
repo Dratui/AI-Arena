77e26e7dc5f23c8f855519959602c7cf574a6c70
2018louysa <arnaud.louys@student-cs.fr>
2018-11-22 09:41:17 +0100

    fixed random_ai test

--- tests/test_random_ai.py
+++ tests/test_random_ai.py
@@ -1,5 +1,6 @@
 import src.games.games as Games
 import AI.random_ai as AI
+from src.board import *
 from pytest import *
 
 def test_ai_output():
@@ -9,6 +10,5 @@ def test_ai_output():
     assert AI.ai_output([[None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [2, None, None, 2, None, None]], game) in [0,1,2,3,4,5]
     game = Games.init_game("ttt")
     assert AI.ai_output([[None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [2, None, None, 2, None, None]], game) in range(36)
-    game.list_board[0].__grid = [[0, 0, 0], [None, 0, 0], [0, 0, 0]]
-    assert AI.ai_output([[0, 0, 0], [None, 0, 0], [0, 0, 0]], game) == 6
-    
+    game.list_board[0]= generate_board_from_list([[0, 0, 0], [None, 0, 0], [0, 0, 0]])
+    assert AI.ai_output([[0, 0, 0], [None, 0, 0], [0, 0, 0]], game) == 3
