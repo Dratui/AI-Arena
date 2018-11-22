7dfc7ccad9e615a2af52d086fdc9f065028fb4f9
Quentin Mondon-Cancel <M.Cancel.Quentin@gmail.com>
2018-11-22 09:32:35 +0000

    Ajout de tests

---
+++ tests/test_random_ai.py
@@ -0,0 +1,14 @@
+import src.games.games as Games
+import AI.random_ai as AI
+from pytest import *
+
+def test_ai_output():
+    game = Games.init_game("2048")
+    assert AI.ai_output([[None, None, None, None], [None, None, None, None], [None, None, None, None], [2, None, None, 2]], game) in [0,1,2,3]
+    game = Games.init_game("p4")
+    assert AI.ai_output([[None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [2, None, None, 2, None, None]], game) in [0,1,2,3,4,5]
+    game = Games.init_game("ttt")
+    assert AI.ai_output([[None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [None, None, None, None, None, None], [2, None, None, 2, None, None]], game) in range(36)
+    game.list_board[0].__grid = [[0, 0, 0], [None, 0, 0], [0, 0, 0]]
+    assert AI.ai_output([[0, 0, 0], [None, 0, 0], [0, 0, 0]], game) == 6
+    
